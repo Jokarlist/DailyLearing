e4d1fc6abe1a4edcad3ce54dff77f714b04c7132
heungkuen <purebloodgk@gmail.com>
2021-12-17 22:48:55 +0800

    reset

--- 01_JS收官/src/js/filter/filterBySex.js
+++
@@ -1,9 +0,0 @@
-var filterBySex = function (sexStr, arr) {
-  if (sexStr === "all") {
-    return arr;
-  } else {
-    return arr.filter(function (elem) {
-      return sexStr.indexOf(elem.sex) !== -1;
-    })
-  }
-};
--- 01_JS收官/src/js/filter/filterByText.js
+++
@@ -1,9 +0,0 @@
-var filterByText = function (filterText, arr) {
-  if (!filterText) {
-    return arr;
-  } else {
-    return arr.filter(function (elem) {
-      return elem.name.indexOf(filterText) !== -1;
-    })
-  }
-};
