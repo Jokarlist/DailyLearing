824c585a66f867f007c01f85107dcdebedc88adb
heungkuen <purebloodgk@gmail.com>
2021-12-23 20:09:05 +0800

    new set test

---
+++ 07_ES6/更多的集合类型/set集合/mySet.js
@@ -0,0 +1,69 @@
+const isEqual = Symbol();
+
+class MySet {
+  constructor(iterator = []) {
+    if (!typeof iterator[Symbol.iterator] === "function") {
+      throw new Error(`传入的${iterator}参数不是一个可迭代对象`);
+    }
+
+    this._datas = [];
+    for (const item of iterator) {
+      this.add(item);
+    }
+  }
+
+  get size() {
+    return this._datas.length;
+  }
+
+  add(data) {
+    if (!this.has(data)) {
+      this._datas.push(data);
+    }
+  }
+
+  delete(data) {
+    for (let i = 0, len = this._datas.length; i < len; i++) {
+      if (this[isEqual](this._datas[i], data)) {
+        this._datas.splice(i, 1);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  has(data) {
+    for (const item of this._datas) {
+      if (this[isEqual](item, data)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  [isEqual](data1, data2) {
+    if (data1 === 0 && data2 === 0) {
+      return true;
+    }
+
+    return Object.is(data1, data2);
+  }
+
+  *[Symbol.iterator]() {
+    for (const item of this._datas) {
+      yield item;
+    }
+  }
+
+  forEach(callback) {
+    for (const item of this._datas) {
+      callback(item, item, this);
+    }
+  }
+
+  clear() {
+    this._datas.length = 0;
+  }
+}
