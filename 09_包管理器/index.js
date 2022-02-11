ca77f11a70674c007281393fd37f05985be9ada5
heungkuen <purebloodgk@gmail.com>
2022-02-11 23:49:59 +0800

    fix bugs

--- 09_包管理器/index.js
+++ 09_包管理器/index.js
@@ -2,7 +2,7 @@ const getNovels = require("./getNovels");
 const fs = require("fs");
 
 getNovels().then((novels) => {
-  fs.writeFile("novels.json", JSON.stringify(novels), () => {
+  fs.writeFile("./录播课/DailyLearing/09_包管理器/novels.json", JSON.stringify(novels), () => {
     console.log("成功获取数据！");
   });
 });
