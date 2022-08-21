7ced22d043812998608ab6f6557ba79280487324
Kuen Heung <57495753+Jokarlist@users.noreply.github.com>
2022-08-21 15:35:41 +0800

    Delete 11_css工程化/抽离css文件 directory
    删除11_css工程化文件夹

--- 11_css工程化/抽离css文件/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  map: false,
-  plugins: {
-    "postcss-preset-env": {
-      stage: 0,
-      preserve: false,
-    },
-  },
-};
--- 11_css工程化/抽离css文件/src/index.js
+++
@@ -1,9 +0,0 @@
-import indexStyles from "./assets/index.pcss";
-import bStyles from "./assets/b.pcss";
-// import "./assets/b.pcss";
-
-const div = document.createElement("div");
-
-console.log(bStyles);
-div.className = bStyles.img;
-document.body.appendChild(div);
