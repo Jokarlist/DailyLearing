61bcb74336dd16b8542d3bb7ffd9523252ac3869
heungkuen <purebloodgk@gmail.com>
2022-08-21 15:30:18 +0800

    修改一些细节

--- 13_Vue2/my-site/src/mock/setting.js
+++ 13_Vue2/my-site/src/mock/setting.js
@@ -5,7 +5,7 @@ Mock.mock("/api/setting", "get", {
   msg: "",
   data: {
     avatar: "https://img0.baidu.com/it/u=3055514396,663073071&fm=253",
-    siteTitle: "高岭之皂の洞穴",
+    siteTitle: "高岭之皂の小屋",
     github: "https://github.com/Jokarlist",
     qq: "1441474779",
     qqQrCode:
