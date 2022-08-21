96e6fe0ff4e9e66f50eecaf13dea1ce65e0f0b99
heungkuen <purebloodgk@gmail.com>
2022-08-21 15:48:22 +0800

    new geek-information project

---
+++ HBuilderProjects/geek-information/pages/follow/follow.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text>关注</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ HBuilderProjects/geek-information/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="container">
+		<text>left</text>
+		<text>center</text>
+		<text>right</text>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+	@include flex(row, space-around);
+}
+</style>
